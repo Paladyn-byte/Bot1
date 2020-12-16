0962a77ef6e7255a49a1abdaf87cab80d8f40137
Udokaj <korrsoft@wp.pl>
2020-12-16 22:52:35 +0100

    Create Konduktor.js

---
+++ Konduktor.js
@@ -0,0 +1,31 @@
+
+
+
+const Discord = require('discord.js')
+const client = new Discord.Client()
+require('dotenv').config();
+
+client.on('ready', () =>{
+console.log('ON_line')
+})
+
+client.login(process.env.BOTTOKEN)
+
+client.on('message', gotMessage);
+
+function gotMessage(msg){
+    console.log(msg.content);
+    if (msg.content === 'test')
+    {
+        msg.reply('Yo Brachu');
+    
+    }
+    if(msg.channel.id=='726507915879514174' && msg.content === 'test1')
+    {
+        msg.reply('Udało się Gratulacje!');
+    }
+    if(msg.channel.id=='726507915879514174' && msg.content === 'test2')
+    {
+        msg.channel.send('tTakie tam ');
+    }
+}
